6179f71b8363cb7d95a47548cdcd86df08589c66
Bram Gussekloo <bram.gussekloo@gmail.com>
2019-09-26 14:39:41 +0200

    intitial commit 2

---
+++ src/main/java/com/bramgussekloo/projects/ProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.bramgussekloo.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bramgussekloo/projects/ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bramgussekloo.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
