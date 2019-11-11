8ccd36807d6db2fffbf5242a669c77eed387d3b8
Guan Hao Wu <guanhao_wu@hotmail.com>
2019-11-11 15:19:45 +0100

    WIP: Reading file from source

---
+++ Back-end/src/main/java/com/bramgussekloo/projects/controller/xlsxReadingController.java
@@ -0,0 +1,25 @@
+package com.bramgussekloo.projects.controller;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+// Controller for xlsx reader
+@Api (value = "Keuzevakken lijst")
+@RestController
+@RequestMapping("/api/kv-lijst")
+public class xlsxReadingController {
+
+
+
+
+}
+
---
+++ Back-end/src/main/java/com/bramgussekloo/projects/statements/XlsxReadingStatements.java
@@ -0,0 +1,24 @@
+package com.bramgussekloo.projects.statements;
+
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class XlsxReadingStatements {
+}
