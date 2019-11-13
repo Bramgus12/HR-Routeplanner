9ea7a04e8d9f48aff80d7e431160673067971deb
Bram Gussekloo <bram.gussekloo@gmail.com>
2019-11-13 13:26:06 +0100

    added that you can look for a folder

--- Back-end/src/main/java/com/bramgussekloo/projects/FileHandling/FileService.java
+++ Back-end/src/main/java/com/bramgussekloo/projects/FileHandling/FileService.java
@@ -1,6 +1,5 @@
 package com.bramgussekloo.projects.FileHandling;
 
-import com.bramgussekloo.projects.statements.LocationNodeNetworkStatements;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,20 +11,19 @@ import java.nio.file.Paths;
 
 @Service
 public class FileService {
-    public static void uploadFile(MultipartFile file) throws FileException, IOException {
-        System.out.println(FileService.class.getResource("FileService.class").toString().toLowerCase());
+    public static void uploadFile(MultipartFile file, String folder) throws FileException, IOException {
         if (FileService.class.getResource("FileService.class").toString().toLowerCase().contains("file")) {
-            Path copyLocation = Paths.get("src/main/resources/Locations/" + file.getOriginalFilename());
+            Path copyLocation = Paths.get("src/main/resources/" + folder + "/" + file.getOriginalFilename());
             Files.copy(file.getInputStream(), copyLocation);
             OutputStream os = Files.newOutputStream(copyLocation);
             os.write(file.getBytes());
         } else if (FileService.class.getResource("FileService.class").toString().toLowerCase().contains("jar")) {
-            Path copyLocation = Paths.get("/usr/share/hr-routeplanner/ProjectC/Back-end/src/main/resources/Locations/" + file.getOriginalFilename());
+            Path copyLocation = Paths.get("/usr/share/hr-routeplanner/ProjectC/Back-end/src/main/resources/" + folder + "/" + file.getOriginalFilename());
             Files.copy(file.getInputStream(), copyLocation);
             OutputStream os = Files.newOutputStream(copyLocation);
             os.write(file.getBytes());
         } else {
-            throw new FileException("file could not be made");
+            throw new FileException("An error occurred.");
         }
     }
 }
