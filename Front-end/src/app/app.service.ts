64eed0c38863efc5e6f8fb62bf118ecc53e4eeac
Matthbo <matthijs.booman@hotmail.com>
2019-11-27 16:00:12 +0100

    Removed homepage service in favour of added app service functions

--- Front-end/src/app/app.service.ts
+++ Front-end/src/app/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable, EventEmitter, Renderer2, RendererFactory2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Building, Node } from './shared/dataclasses';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +13,11 @@ export class AppService {
   darkMode: boolean = false;
 
   private renderer: Renderer2;
+  private API_URL = "/api/";
+  private buildings: Building[] = [];
+  private rooms: Node[] = [];
 
-  constructor(rendererFactory: RendererFactory2) { 
+  constructor(private http: HttpClient, rendererFactory: RendererFactory2) { 
     this.renderer = rendererFactory.createRenderer(null, null);
   }
 
@@ -19,6 +26,44 @@ export class AppService {
    */
   toggle(){
     this.trigger.next(null);
+  }
+
+  /**
+   * Returns an Observable with Building[], first retrieves it from the API if the array is empty
+   * This Observable is finite, no need to unsubscribe unless you want to cancel
+   */
+  getBuildings(){
+    return new Observable<Building[]>(subscriber => {
+      if(this.buildings.length == 0){
+        this.http.get<Building[]>(this.API_URL + "building").subscribe(data => {
+          this.buildings = data;
+          subscriber.next(this.buildings);
+          subscriber.complete();
+        });
+      } else {
+        subscriber.next(this.buildings);
+        subscriber.complete();
+      }
+    });
+  }
+
+  /**
+   * Returns Observable with Node[], first retrieves it from the API if the array is empty
+   * This Observable is finite, no need to unsubscribe unless you want to cancel
+   */
+  getRoomNodes(){
+    return new Observable<Node[]>(subscriber => {
+      if(this.rooms.length == 0){
+        this.http.get<Node[]>(this.API_URL + "locationnodenetwork/room").subscribe(data => {
+          this.rooms = data;
+          subscriber.next(this.rooms);
+          subscriber.complete();
+        });
+      } else {
+        subscriber.next(this.rooms);
+        subscriber.complete();
+      }
+    });
   }
 
   /**
