3e4cdb0ebd7a9c3d9362592494ef2962382ed033
Pedro Bronsveld <pedrobronsveld@gmail.com>
2019-12-12 01:47:20 +0100

    Rewrote convertPdfs.py so rotated room codes can now be extracted.

--- Blender/buildings/convertPdfs.py
+++ Blender/buildings/convertPdfs.py
@@ -8,14 +8,9 @@ from pdf2image.exceptions import (
 )
 import re
 import json
-from pdfminer.pdfparser import PDFParser, PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine
-import pdfminer
+import tempfile
 from PyPDF2 import PdfFileReader
+import xml.etree.ElementTree as ET
 
 def path_to_extension(path, extension, extra=""):
     return ".".join(path.split(".")[:-1]) + extra + "." + extension
@@ -30,111 +25,108 @@ for path, subdirs, files in os.walk(root):
             pdfPaths.append(os.path.join(path, name))
 
 pdfNum = 1
+
+tempDir = tempfile.TemporaryDirectory()
+
 for pdfPath in pdfPaths:
     print( "[{}/{}] Converting '{}' to png and json files.".format(pdfNum, len(pdfPaths), os.path.split(pdfPath)[-1] ) )
     pdfNum += 1
 
     floorplanFormat = 'A3'
-
     with open(pdfPath, 'rb') as pdfFile:
         pdf = PdfFileReader(pdfFile)
         info = pdf.getDocumentInfo()
         formatMatch = re.search("^A[0-9]$", info.title)
         if formatMatch != None:
-            floorplan['format'] = formatMatch.group(0)
+            floorplanFormat = formatMatch.group(0)
 
-    # Extract text and text position with pdf miner
-    with open(pdfPath, 'rb') as pdf_file:
-        parser = PDFParser(pdf_file)
-        document = PDFDocument(parser)
-        parser.set_document(document)
-        document.set_parser(parser)
-        document.initialize()
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams()
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
+    xmlPath = os.path.join( tempDir.name, os.path.basename(path_to_extension(pdfPath, "xml" )) )
 
-        pagesDimensions = list(document.get_pages())
-        pageNum = 1
-        for dimensions in pagesDimensions:
-            pathExtra = "-p" + str(pageNum) if len(pagesDimensions) > 1 else ""
-            jsonPath = path_to_extension(pdfPath, "json", extra=pathExtra )
-            if len(pagesDimensions) > 1:
-                print("\tpage {}/{}".format(pageNum, len(pagesDimensions)))
-            pageNum += 1
-            if os.path.isfile(jsonPath):
-                print(jsonPath, "already exists, skipping.")
-                continue
+    subprocess.call(["pdftohtml", "-xml", pdfPath, xmlPath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-            floorplan = {
-                'buildingName': '',
-                'width': 0,
-                'height': 0,
-                'orientation': 'Landscape',
-                'scale': 100.0,
-                'floorNumberDetected': False,
-                'floorNumber': 0,
-                'rooms': [],
-                'format': formatMatch
-            }
+    xmlTree = ET.parse(xmlPath)
+    xmlRoot = xmlTree.getroot()
 
-            floorplan['width'] = dimensions.mediabox[2]
-            floorplan['height'] = dimensions.mediabox[3]
+    pageNum = 1
+    pageElements = [element for element in xmlRoot if element.tag == 'page']
+    for pageElement in pageElements:
 
-            interpreter.process_page(dimensions)
-            layout = device.get_result()
-            detectedFloorNumbers = dict()
+        pathExtra = "-p" + str(pageNum) if len(pageElements) > 1 else ""
+        jsonPath = path_to_extension(pdfPath, "json", extra=pathExtra )
+        if len(pageElements) > 1:
+            print("\tpage {}/{}".format(pageNum, len(pageElements)))
+        pageNum += 1
+        if os.path.isfile(jsonPath):
+            print(jsonPath, "already exists, skipping.")
+            continue
 
-            for lt_obj in layout:
-                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-                    text = lt_obj.get_text()
-                    roomMatch = re.search(r"[0-9A-Z]+\.[\-0-9A-Za-z]+\.[0-9A-Za-z]+", text)
-                    if(roomMatch != None):
-                        if(floorplan['buildingName'] == ''):
-                            floorplan['buildingName'] = roomMatch.group(0).split('.')[0]
+        floorplan = {
+            'buildingName': '',
+            'width': int(pageElement.attrib.get('width')),
+            'height': int(pageElement.attrib.get('height')),
+            'orientation': 'Landscape',
+            'scale': 100.0,
+            'floorNumberDetected': False,
+            'floorNumber': 0,
+            'rooms': [],
+            'format': floorplanFormat
+        }
 
-                        floorMatch = re.search("[-0-9]+", roomMatch.group(0).split('.')[1])
-                        if floorMatch != None or roomMatch.group(0).split('.')[1] == 'K':
-                            floorNumber = -1 if roomMatch.group(0).split('.')[1] == 'K' else int(floorMatch.group(0))
-                            if detectedFloorNumbers.get(floorNumber) == None:
-                                detectedFloorNumbers[floorNumber] = 1
-                            else:
-                                detectedFloorNumbers[floorNumber] += 1
-                        # The text object is a room, save the corresponding data
-                        room = {
-                            'code': roomMatch.group(0),
-                            'label': re.sub(r'\n[\s\S]*[0-9]+[\s\S]*|[\s\S]*[0-9] m2[\s\S]*', '', text[roomMatch.span()[1]:].strip() ),
-                            'x1': lt_obj.bbox[0],
-                            'y1': lt_obj.bbox[1],
-                            'x2': lt_obj.bbox[2],
-                            'y2': lt_obj.bbox[3]
-                        }
-                        floorplan['rooms'].append(room)
-                    
-                    scaleMatch = re.search("(?<=Schaal) *1 *: *[0-9]*( *, *[0-9]*)?", text, re.IGNORECASE)
-                    if(scaleMatch != None):
-                        # The text object contains the floor plan scale
-                        floorplan['scale'] = float(scaleMatch.group(0).split(':')[-1].replace(',','.'))
+        detectedFloorNumbers = dict()
 
-            if len(detectedFloorNumbers) > 0:
-                floorplan['floorNumber'] = max(detectedFloorNumbers)
-                floorplan['floorNumberDetected'] = True
-            
-            # Save floorplan data to a json file
-            jsonFile = open(jsonPath, 'w')
-            json.dump(floorplan, jsonFile)
-            jsonFile.close()
+        for textElement in [element for element in pageElement if element.tag == 'text' and element.text != None]:
+            text = textElement.text
+            roomMatch = re.search(r"[0-9A-Z]+\.[\-0-9A-Za-z]+\.[0-9A-Za-z]+", text)
+            if(roomMatch != None):
+                if(floorplan['buildingName'] == ''):
+                    floorplan['buildingName'] = roomMatch.group(0).split('.')[0]
 
-            # save the floor plan as a png file
-            pngPath = path_to_extension(pdfPath, "png", extra=pathExtra)
-            image = convert_from_path(pdfPath, size=(5000, None), first_page=pageNum-1)[0].convert("RGBA")
-            # make background transparent
-            pixdata = image.load()
-            for y in range(image.size[1]):
-                for x in range(image.size[0]):
-                    if pixdata[x, y] == (255, 255, 255, 255):
-                        pixdata[x, y] = (0, 0, 0, 0)
+                floorMatch = re.search("[-0-9]+", roomMatch.group(0).split('.')[1])
+                if floorMatch != None or roomMatch.group(0).split('.')[1] == 'K':
+                    floorNumber = -1 if roomMatch.group(0).split('.')[1] == 'K' else int(floorMatch.group(0))
+                    if detectedFloorNumbers.get(floorNumber) == None:
+                        detectedFloorNumbers[floorNumber] = 1
                     else:
-                        pixdata[x, y] = (0, 255, 255, 255)
-            image.save(pngPath)
+                        detectedFloorNumbers[floorNumber] += 1
+                # The text object is a room, save the corresponding data
+                room = {
+                    'code': roomMatch.group(0),
+                    'label': '',
+                    'x1': int(textElement.attrib.get('left')),
+                    'y1': floorplan['height'] - int(textElement.attrib.get('top')),
+                    'x2': int(textElement.attrib.get('left')) + int(textElement.attrib.get('width')),
+                    'y2': floorplan['height'] - int(textElement.attrib.get('top')) + int(textElement.attrib.get('height'))
+                }
+                floorplan['rooms'].append(room)
+            
+            scaleMatch = re.search("(?<=Schaal) *1 *: *[0-9]*( *, *[0-9]*)?", text, re.IGNORECASE)
+            if(scaleMatch != None):
+                # The text object contains the floor plan scale
+                floorplan['scale'] = float(scaleMatch.group(0).split(':')[-1].replace(',','.'))
+            
+        if len(detectedFloorNumbers) > 0:
+            floorplan['floorNumber'] = max(detectedFloorNumbers)
+            floorplan['floorNumberDetected'] = True
+
+        # Save floorplan data to a json file
+        jsonFile = open(jsonPath, 'w')
+        json.dump(floorplan, jsonFile)
+        jsonFile.close()
+        
+        # save the floor plan as a png file
+        pngPath = path_to_extension(pdfPath, "png", extra=pathExtra)
+        if os.path.isfile(pngPath):
+            print(pngPath, "already exists, skipping.")
+            continue
+        image = convert_from_path(pdfPath, size=(5000, None), first_page=pageNum-1)[0].convert("RGBA")
+        # make background transparent
+        pixdata = image.load()
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                if pixdata[x, y] == (255, 255, 255, 255):
+                    pixdata[x, y] = (0, 0, 0, 0)
+                else:
+                    pixdata[x, y] = (0, 255, 255, 255)
+        image.save(pngPath)
+
+tempDir.cleanup()
